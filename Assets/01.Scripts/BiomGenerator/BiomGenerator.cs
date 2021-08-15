d15a4d3f7a1e6e0fb6220e67c13cbc7f45e27747
PassingG <49996889+PassingG@users.noreply.github.com>
2021-08-15 17:33:31 +0900

    Add Burst, Make BaseSingleton
    Add Burst, Make BaseSingleton

---
+++ Assets/01.Scripts/BiomGenerator/BiomGenerator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.Mathematics;
+using UnityEngine.UI;
+
+public class BiomGenerator : MonoBehaviour
+{
+    
+}
---
+++ Assets/01.Scripts/Utillity/BaseSingleton.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public interface IBaseSingleton
+{
+    void OnCreateInstance();
+    void OnDestroyInstance();
+}
+
+public abstract class BaseSingleton<T> where T : class, IBaseSingleton, new()
+{
+    private static T instance;
+    public static T Instance -> instance;
+
+    public static T CreateInstance()
+    {
+        if(instance == null)
+        {
+            instance = new T();
+            instance.OnCreateInstance();
+        }
+        return instance;
+    }
+
+    public static void DestroyInstance()
+    {
+        Instance?.OnDestroyInstance();
+        instance = null;
+    } 
+}
+
+public abstract class BaseMonoSingleton<T> : MonoBehaviour where T : class
+{
+    public static T Instance { get; private set; }
+
+    [SerializeField]
+    private bool m_IsDontDestroyObject = false;
+    public bool isDontDestroyObject => m_IsDontDestroyObject;
+
+    public static T1 GetInstance<T1>() where T1 : class, T
+    {
+        return Instance as T1;
+    }
+
+    protected virtual void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this as T;
+            if (isDontDestroyObject) DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(Instance.Equals(this))
+        {
+            Instance = null;
+        }
+    }
+}
